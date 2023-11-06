c56c5d12b4cf5a7bc8facefb9beb06fdc266f8e1
Mogeum <148171018+Mogeum@users.noreply.github.com>
2023-11-06 14:01:39 +0900

    최종

--- js/sub5-1.js
+++ js/sub5-1.js
@@ -1,8 +1,8 @@
-const eventList = document.querySelector('#event-list')
-const button = document.querySelector('button')
-button.addEventListener("click",() =>{
+const eventList = document.querySelector('#event-list');
+const button = document.querySelector('button');
+button.addEventListener("click", () => {
   let eventText = eventList.value;
-  if (eventText == ""){
-    alert(`문자를 입력해주세요!`)
+  if (eventText == "") {
+    alert(`문자를 입력해주세요!`);
   }
 });
